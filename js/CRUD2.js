f400966ff8d56ac63730807ef125233f0a412aa3
Your Name <duy01657853612@gmail.com>
2022-08-29 16:54:33 +0700

    afternoon 29_8

--- js/CRUD2.js
+++ js/CRUD2.js
@@ -1,4 +1,4 @@
-var postsAPI = "http://localhost:3000/posts";
+let postsAPI = "http://localhost:3000/posts";
 
 // START
 function start() {
@@ -17,7 +17,7 @@ function getPosts(callback) {
 }
 // CREATE
 function createPost(data, callback) {
-  var options = {
+  let options = {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
@@ -33,7 +33,7 @@ function createPost(data, callback) {
 }
 // DELETE
 function handleDeletePost(id) {
-  var options = {
+  let options = {
     method: "DELETE",
     headers: {
       "Content-Type": "application/json",
@@ -45,7 +45,7 @@ function handleDeletePost(id) {
       response.json();
     })
     .then(function () {
-      // var dele = document.querySelecttor(".post-item-" + id);
+      // let dele = document.querySelecttor(".post-item-" + id);
       // if (dele) {
       //   dele.remove();
       // }
@@ -55,43 +55,37 @@ function handleDeletePost(id) {
 
 //RENDER
 function renderPosts(posts) {
-  var listPost = document.querySelector("#list-posts");
-  var htmls = posts.map(function (post) {
+  let listPost = document.querySelector("#list-posts");
+  let htmls = posts.map(function (post) {
     return `
-    <table class='container-fluid text-center'>
-        <tr class='row post-item-${post.id}'>
-          <th class='col-4'>
-            ${post.title}
-          </th>
-          <th class='col-4'>
-            ${post.author}
-          </th>
-
-          <th class='col-4'>
-          <button  onClick="handleDeletePost(${post.id})">Xóa
-          <button onClick="handleFixPost(${post.id})">Sửa
-          </button>
-          </button>
-          </th>
-          <th class='col-2' >
-           
-          </th>
-        </tr>
-        </table >
-        `;
+    <table class="table">
+      <tr>
+            <th scope="row">${post.id}</th>
+            <td>${post.title}</td>
+            <td> ${post.author}</td>
+            <td>
+              <button type="button" class="btn btn-primary " onClick="handleDeletePost(${post.id})">Xóa</button>
+              <button type="button" class="btn btn-secondary" onClick="handleFixPost(${post.id})">Sửa</button>
+            </td>
+      </tr>
+    </table>
+    `;
   });
   let titleTB = `
-    <div class="tieuDe container-fluid text-center">
-      <div class="row ">
-            <div class="username col-4">Title </div>
-            <div class="company col-4">Author </div>
-            <div class="company col-4">Chức năng</div>
-      </div>
-        
-    </div>`;
+  <table class="table">
+      <thead>
+        <tr>
+          <th scope="col">#</th>
+          <th scope="col">Title</th>
+          <th scope="col">Author</th>
+          <th scope="col">Chức năng</th>
+        </tr>
+      </thead>
+    </table>`;
   let html = htmls.join("");
   // document.getElementById("demo").innerHTML = titleTB + html;
-  listPost.innerHTML = titleTB + html;
+  // listPost.innerHTML = titleTB + html;
+  listPost.innerHTML = html;
 }
 
 // SỬ LÝ CREATE
@@ -99,10 +93,10 @@ function handleCreateForm() {
   let createBtn = document.querySelector("#create");
   createBtn.onclick = function () {
     // TẠO GIÁ TRỊ TRONG INPUT
-    var title = document.querySelector('input[name="title"]').value;
-    var author = document.querySelector('input[name="author"]').value;
+    let title = document.querySelector('input[name="title"]').value;
+    let author = document.querySelector('input[name="author"]').value;
     // TẠO ĐỐI TƯỢNG
-    var formData = {
+    let formData = {
       title: title,
       author: author,
     };
@@ -116,7 +110,30 @@ function handleCreateForm() {
 
 // SỬA
 
-function handleFixPost(data, callback) {
+// function handleFixPost(data, callback) {
+//   const formData = new FormData();
+//   const fileField = document.querySelector('input[name="title"]');
+
+//   formData.append("123", "abc123");
+//   formData.append("avatar", fileField.files[0]);
+//   let options1 = {
+//     method: "PUT",
+//     body: formData,
+//   };
+//   fetch(postsAPI, options1)
+//     .then(function (response) {
+//       response.json();
+//     })
+//     // .then(callback)
+//     .then(function (result) {
+//       console.log("Success:", result);
+//     })
+//     .catch((error) => {
+//       console.error("Error:", error);
+//     });
+// }
+
+function handleFixPost(id) {
   let options1 = {
     method: "PATCH",
     // method: "POST",
@@ -128,24 +145,33 @@ function handleFixPost(data, callback) {
       // Accept: "application/json",
       // "Content-Type": "application/json",
       // _method: "PATCH",
-      "Content-Type": "application/json",
+      "Content-Type": "application/json;  charset=UTF-8",
       // 'Content-Type': 'application/x-www-form-urlencoded',
     },
-    body: JSON.stringify(data),
+    // body: JSON.stringify(data),
   };
   fetch(postsAPI + "/" + id, options1)
     .then(function (response) {
       response.json();
     })
-    .then(callback);
+    .then(function () {
+      getPosts(renderPosts);
+    });
+  // .then(function (data) {
+  //   console.log(data);
+  // })
+  // .catch(function () {
+  //   console.log("lỗi");
+  // });
+  // .then(callback);
 }
 
 function inputFix(id) {
-  var iFix = document.querySelector("#list-posts");
+  let iFix = document.querySelector("#save");
   iFix.onclick = function () {
-    var title = document.querySelector('input[name="title"]').innerHTML;
-    var author = document.querySelector('input[name="author"]').innerHTML;
-    var dataForm1 = {
+    let title = document.querySelector('input[name="title"]').value;
+    let author = document.querySelector('input[name="author"]').value;
+    let dataForm1 = {
       title: title,
       author: author,
     };
